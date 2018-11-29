7f2d247195a19b1066359089ba4144824efd3a6e
Andrea Jurado <avjh_18@hotmail.com>
2018-11-29 02:09:00 -0500

    Modificacion index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,34 @@
 
+var sonido_bg =document.getElementById('background-music');
+sonido_bg.volumen = 0.5;
+sonido_bg.play();
+
+var escogePersonaje =document.getElementById('elige-personaje');
+escogePersonaje.volume =1;
+escogePersonaje.play();
+
 //accion.addEventListener("click", colocarAvatar);
 
 var conten=document.getElementById("contenAvatares");
 var datos=JSON.parse(localStorage.getItem("avatares"));
 var mostra="";
-  var tblBody = document.createElement("tbody");
-    var hilera = document.createElement("tr");
+  
 for (var img in datos) {
-  var celda = document.createElement("td");
+  var celda = document.createElement("div");
   var puntos= document.createElement("h4");
   puntos.textContent=datos[img].puntos+" puntos";
   var imagenAvatar = document.createElement("img");
- celda.id="tabla";
-  celda.className='personal';
+
   imagenAvatar.src='../img/'+datos[img].nombre+'';
   imagenAvatar.setAttribute("onclick","sesion("+img+")");
-  imagenAvatar.className='rounded-circle  img-responsive';
   var link = document.createElement("a");
   link.href='instrucciones.html';
-  //conten.innerHTML+="<td onclick='sesion("+datos[img]+")' id='tabla'><a  href='instrucciones.html?"+datos[img].nombre+"'  id='"+datos[img].id+"'><img class='rounded-circle' id='imgen' src='../img/"+datos[img].nombre+"''></a></td>";
+  
 celda.appendChild(puntos);
 celda.appendChild(link);
 
 link.appendChild(imagenAvatar);
-hilera.appendChild(celda);
+conten.appendChild(celda); //agrega los avatares
 
 //console.log(datos[img]);
 }
